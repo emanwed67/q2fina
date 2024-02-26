6c02345decd565434d3fe1ff9ef99c9b2b7903f1
eman wed <emanwed67@gmail.com>
2024-02-26 22:38:04 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+import 'quick_survey.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:Quick() ,
+    );
+  }
+}
